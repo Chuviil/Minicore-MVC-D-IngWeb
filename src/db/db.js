e3e59da2d53725a2892d9d007a771f9f6b88931d
Sebastian Quito <josue.quito@udla.edu.ec>
2025-01-25 20:19:42 -0500

    implemented functionality with joins

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,14 @@
 import Database from "better-sqlite3";
 
-const db = new Database('minicore.db');
-  
-export default db;
+export function getSalesByDateRange(fromDate, toDate) {
+  const db = new Database('minicore.db');
+
+  const stmt = db.prepare(`
+    SELECT Departments.name AS department, SUM(Sales.amount) AS total
+    FROM Sales
+    JOIN Departments ON Sales.department_id = Departments.id
+    WHERE Sales.date BETWEEN ? AND ?
+    GROUP BY Departments.name
+  `);
+  return stmt.all(fromDate, toDate);
+}
